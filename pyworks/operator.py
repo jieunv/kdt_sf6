926caee846bad8ddfb3b60dcdf8246b4e70a8782
jieunv <jeongjieun0610@gmail.com>
2024-08-01 17:17:33 +0900

    파이썬 소스 프로그램

---
+++ pyworks/operator.py
@@ -0,0 +1,37 @@
+# 연산자(operator)
+# 산술 연산 - +, -, *, /, //(몫), %(나머지)
+n1 = 10
+n2 = 4
+
+#산술연산
+sum = n1 + n2
+sub = n1 - n2
+mul = n1 * n2
+dev = n1 / n2
+
+
+#출력 
+print(n1, n2)   
+print(sum)  
+print(sub)  
+print(mul)  
+print(dev)  
+print(n1 // n2)
+print(n1 % n2)  
+
+
+print(n1 ** n2) # 10000 거듭제곱
+print(2 ** 3)
+
+#실습
+
+b1 = 30
+p1 = 4
+
+몫 = b1 // p1
+나머지 = b1 % p1
+
+print("빵의 개수 : ", 몫)
+print("남은 빵의 개수 : ", 나머지)
+
+
---
+++ pyworks/var2.py
@@ -0,0 +1,41 @@
+""" - 여러줄 주석 가능
+변수 선언(변수명 문법)
+- 숫자로 시작하면 안됨
+- 공백이 있으면 안됨
+- 공백문자가 있으면 안됨
+- 특수문자가 있으면 안됨(몇개 되는건 있지만 거의 안됨) 허용되는 것들('_' 언더바 가능)
+
+- 에러의 종류
+1. 실행 전 에러(컴파일오류) - 에디터가 미리 알려줌
+2. 실행 후 에러(런타임에러) - 
+
+
+"""
+
+# season2 = "여름"
+# season = summer
+# 2season = "여름"
+# season 2 = "summer"
+#age = 13
+
+
+#print(season2)
+#print(age)
+
+# 연습
+name = 'harin'
+name = 'victoria'
+print(name)
+
+print('***회원가입***')
+user_id = 'jeong6'
+user_pw = 'sf1234'
+email = 'jeongjieun0610@gmail.com'
+age = '23'
+
+print("아이디: ", user_id)
+print("비밀번호: ", user_pw)
+print("이메일: ", email)
+print("나이: ", age)
+
+
