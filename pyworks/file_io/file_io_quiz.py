48b659d10b5104658e15ef1c471182aef6fe52a6
jieunv <jeongjieun0610@gmail.com>
2024-08-14 20:55:14 +0900

    database

--- pyworks/file_io/file_io_quiz.py
+++ pyworks/file_io/file_io_quiz.py
@@ -1,5 +1,6 @@
 # 실습 1 - 구구단
 
+'''
 f = open("c:/pyfile/gugudan.txt", "a")
 
 for i in range(2, 10):
@@ -13,3 +14,19 @@ f = open("c:/pyfile/gugudan.txt", "r")
 gugu = f.read()
 print(gugu)
 f.close()
+'''
+
+# 실습2
+try:
+    with open("./source/member.txt", 'w') as f:
+        for text in range(3):
+            name = input("이름을 입력하세요 : ")
+            pw = input("비밀번호를 입력하세요 : ")
+            f.write(f'{name} {pw}\n')
+
+except FileNotFoundError:
+    print("파일을 찾을 수 없습니다.")
+
+
+# 실습 3
+
